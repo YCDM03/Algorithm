bc51e24dd7a551419577640ec98847124af3b2df
YCDM03 <164147591+YCDM03@users.noreply.github.com>
2024-05-03 11:53:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,6 +1,5 @@
  function solution(absolutes, signs) {
-      let result = absolutes.reduce((acc, e, i) => {
-        return signs[i] == true ? acc + e : acc - e;
-      }, 0);
-      return result;
+ return absolutes.reduce((acc,e,i)=>{
+       return signs[i]==true?acc+=e:acc-=e
+     },0)
     }
