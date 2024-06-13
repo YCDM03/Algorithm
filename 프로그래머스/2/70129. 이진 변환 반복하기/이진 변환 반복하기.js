038016eecb3f9c0ccdb4009aef0034e8d017cfbe
YCDM03 <164147591+YCDM03@users.noreply.github.com>
2024-06-13 11:02:06 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.61 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,22 +1,10 @@
-const solution = (s = "") => {
-  let itrCount = 0,
-    zeroCount = 0;
-  let answer = [];
-  if (s !== "1") {
-    let arr = s.split("");
-    while (arr.join("") !== "1") {
-      itrCount++;
-      let temp = [];
-      arr.map((value) => (value === "0" ? zeroCount++ : temp.push("1")));
-      let len = temp.length;
-      let newBin = [];
-      while (len != 0) {
-        newBin.splice(0, 0, (len % 2).toString());
-        len = (len - (len % 2)) / 2;
-      }
-      arr = Object.assign([], newBin);
+
+function solution(s) {
+    var answer = [0,0];
+    while(s.length > 1) {
+        answer[0]++;
+        answer[1] += (s.match(/0/g)||[]).length;
+        s = s.replace(/0/g, '').length.toString(2);
     }
-  }
-  answer = [itrCount, zeroCount];
-  return answer;
-};
+    return answer;
+}
