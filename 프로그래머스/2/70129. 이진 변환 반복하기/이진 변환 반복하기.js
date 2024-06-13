b4aca8a7ced39e58dd7689fb11ed6ed8ba9c148b
YCDM03 <164147591+YCDM03@users.noreply.github.com>
2024-06-13 10:59:07 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.78 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    let delZeroCount=0
+    let binaryCount=0
+    let vStr=s
+    
+    do{
+        vStr=vStr.split("").map((e)=>{if(e!=="1"){delZeroCount++}else{return e}}).join("").length.toString(2)
+         binaryCount++
+    }while(vStr!=="1")
+    
+    return [binaryCount,delZeroCount]
+}
