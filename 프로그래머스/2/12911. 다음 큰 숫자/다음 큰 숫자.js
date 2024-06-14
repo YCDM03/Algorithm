d1c110affdaff9abaae4e0556bbbd459aee257b7
YCDM03 <164147591+YCDM03@users.noreply.github.com>
2024-06-14 15:38:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer=n+1
+    let oneCount=0
+    let answerOneCount=0
+    n.toString(2).split("").forEach((e)=>{e==="0"?null:oneCount++;})
+    
+    
+    while(true){
+    answer.toString(2).split("").forEach((e)=>{e==="0"?null:answerOneCount++;})
+      if(oneCount===answerOneCount){break}
+        else {answer++; answerOneCount=0}
+    }
+
+     return answer
+}
