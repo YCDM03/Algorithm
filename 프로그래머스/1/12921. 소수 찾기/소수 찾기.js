c6695443dd18ea801d0937c04c3d4a6794330757
YCDM03 <164147591+YCDM03@users.noreply.github.com>
2024-09-12 00:32:19 +0900

    [level 1] Title: 소수 찾기, Time: 55.27 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,17 +1,14 @@
 function solution(n) {
-    if(n<2)return 0
-    
     const arr=Array(n+1).fill(true)
     arr[0]=arr[1]=false
 
-    for (let i = 2; i * i <= n; i++) {
+    for (let i = 2; i <= n; i++) {
         if (arr[i]) {
-            for (let j = i * i; j <= n; j += i) {
-                arr[j] = false;
+            for (let j = 2; j <= n/i; j ++) {
+                arr[j*i] = false;
             }
         }
     }
-    
     let count=0
     arr.forEach((e)=>{if(e){count++}})
     return count
